df3881a82a5e38426b1116f4719ec380e6c85c2c
Florian Maneyrol <florianmaneyrol@gmail.com>
2016-04-11 22:49:02 +0200

    test on devise succes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408135032) do
+ActiveRecord::Schema.define(version: 20160411201653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,10 @@ ActiveRecord::Schema.define(version: 20160408135032) do
     t.integer  "user_id"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.integer  "asso_id"
   end
 
+  add_index "missions", ["asso_id"], name: "index_missions_on_asso_id", using: :btree
   add_index "missions", ["user_id"], name: "index_missions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -73,5 +75,6 @@ ActiveRecord::Schema.define(version: 20160408135032) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "missions", "assos"
   add_foreign_key "missions", "users"
 end
